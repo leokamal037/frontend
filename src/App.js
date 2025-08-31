8e58721c36c0ae32a4e84af8a89e6330e6ead5cb
Leo Prabu <leoprabu8128@gmail.com>
2025-08-31 11:22:42 +0530

    jssr.commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Player from './pages/Player';
+import Forgetpass from './pages/ForgotPassword';
+import Otpsend from './pages/OtpLogin';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/forgetpass" element={<Forgetpass />} />
+        <Route path="/otpsend" element={<Otpsend />} />
+         
+      </Routes>
+    
   );
 }
 
 export default App;
+
